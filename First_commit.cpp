9ab2671025599042adeee3d5bc68a0fb7415e7ce
AndreiLaurentiu <laurentiucojocaru2020@gmail.com>
2021-10-19 20:27:32 +0300

    Al treilea commit pentru tema 1

--- First_commit.cpp
+++ First_commit.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 class Exercitiu{
     private:
@@ -49,9 +50,28 @@ class Exercitiu{
             int MET;
             std::cout<<"Alegerea MET: Exercitiu usor - sub 3" << std::endl << "Exercitiu mediu - intre 3 si 6" << std::endl << "Exercitiu intens - peste 6" <<std::endl;
             std::cin>>MET;
-            std:: cout << durata * (MET*3.5*greutate)/200;
+            std:: cout << "Numarul de calorii arse cu acest exercitiu: " << durata * (MET*3.5*greutate)/200;
         }
 };
+
+class Nutritie{
+    private:
+        float carbohidrati;
+        float grasimi;
+        float fibre;
+        float proteine;
+        float apa;
+    public:
+        Nutritie(float carbohidrati, float grasimi, float fibre, float proteine, float apa) : carbohidrati(carbohidrati), grasimi(grasimi),  fibre(fibre), proteine(proteine), apa(apa){
+            // Suntem in constructorul de initializare
+        }
+        friend std::ostream &operator<<(std::ostream &os, const Nutritie &Nutritie) {
+            os << "Valorile sunt redate in grame." << std::endl << "Carbohidrati: " << Nutritie.carbohidrati << std::endl << "Grasimi: " << Nutritie.grasimi << std::endl << "Fibre: " << Nutritie.fibre << std::endl << "Proteine: " << Nutritie.proteine << std::endl; 
+            return os;
+            // Suntem in operatorul <<
+        }
+};
+
 
 class Utilizator{
     private:
@@ -60,8 +80,10 @@ class Utilizator{
     float inaltime;
     std::string sex;
     std::string nume;
+    Nutritie nutritie;
+    std::vector <Exercitiu> exercitii;
     public:
-    Utilizator(int varsta, float greutate, float inaltime, const std::string &sex, const std::string &nume) : varsta(varsta), greutate(greutate), inaltime(inaltime), sex(sex), nume(nume){
+    Utilizator(int varsta, float greutate, float inaltime, const std::string &sex, const std::string &nume, const Nutritie &nutritie, const std::vector<Exercitiu> exercitii) : varsta(varsta), greutate(greutate), inaltime(inaltime), sex(sex), nume(nume), nutritie(nutritie),  exercitii( exercitii){
         // Suntem in constructorul de initializare
     }
     friend std::ostream &operator<<(std::ostream &os, const Utilizator &Utilizator) {
@@ -70,18 +92,28 @@ class Utilizator{
         os << "Varsta: " << Utilizator.varsta << std::endl;
         os << "Greutate: " << Utilizator.greutate << std::endl;
         os << "Inaltime: " << Utilizator.inaltime << std::endl;
+        os << "Valori nutritionale individuale pentru o zi: " << std::endl << Utilizator.nutritie;
+        os << "Exercitii fizice selectate pentru azi: "<< std::endl;
+        for(int i=0; i<Utilizator.exercitii.size(); i++)
+            os << Utilizator.exercitii[i];
         return os;
          // Suntem in operatorul <<
         }
+    void adauga_exercitiu(Exercitiu exercitiu){
+        exercitii.push_back(exercitiu);
+    }
     float getGreutate() {
         return greutate;
     }
-    int BMR(){
+    int AMR(){
+        float activitate;
+        std::cout<<"Introduceti nivelul de activitate fizica zilnica dupa cum urmeaza: Sedentar(1.2), Putin activ(1.375), Moderat activ(1.55), Activ(1.725), Foarte activ(1.9)"<<std::endl;
         std::cout<<"Numarul zilnic de calorii necesare pentru mentinerea greutatii actuale: ";
+        std::cin>>activitate;
         if(sex == "masculin")
-            return 10*greutate + 6.25*inaltime - 5*varsta + 5;
+            return (10*greutate + 6.25*inaltime - 5*varsta + 5)*activitate;
         else
-            return 10*greutate + 4.7*inaltime - 5*varsta -161;
+            return (10*greutate + 4.7*inaltime - 5*varsta -161)*activitate;
     }
     void IMC(){
         float IMC_result = greutate*std::pow(10, 4)/std::pow(inaltime, 2);
@@ -128,24 +160,6 @@ class Utilizator{
     }
 };
 
-class Nutritie{
-    private:
-        float carbohidrati;
-        float grasimi;
-        float fibre;
-        float proteine;
-        float apa;
-    public:
-        Nutritie(float carbohidrati, float grasimi, float fibre, float proteine, float apa) : carbohidrati(carbohidrati), grasimi(grasimi),  fibre(fibre), proteine(proteine), apa(apa){
-            // Suntem in constructorul de initializare
-        }
-        friend std::ostream &operator<<(std::ostream &os, const Nutritie &Nutritie) {
-            os << "Valorile sunt redate in grame." << std::endl << "Carbohidrati: " << Nutritie.carbohidrati << std::endl << "Grasimi: " << Nutritie.grasimi << std::endl << "Fibre: " << Nutritie.fibre << std::endl << "Proteine: " << Nutritie.proteine << std::endl; 
-            return os;
-            // Suntem in operatorul <<
-        }
-};
-
 int main(){
     std::string nume="Forfecari";
     Exercitiu t(15, 4, 10, nume);
@@ -153,13 +167,15 @@ int main(){
     Exercitiu d=t, z;
     //std::cout<<d; // testam cc-ul
     z = t;
+    Exercitiu k(15, 5, 15, "Genoflexiuni");
+    std::vector <Exercitiu> Exercitii={t, k};
     //std::cout<<z; // testam operatorul de atribuire
-    Utilizator g(20, 90, 185, "masculin", "Andrei");
+    Nutritie x(300.0, 30.0,40.0 ,50.0, 2.0);
+    //std::cout << x; // testam operatorul <<
+    Utilizator g(20, 90.0, 185.0, "masculin", "Andrei", x, Exercitii);
     //std::cout << g; // testam operatorul <<
     //std::cout << g.BMR(); //verificare metoda BMR
     //g.IMC(); //verificare metoda IMC
     // t.nr_total_calorii_arse(g.getGreutate());
-    Nutritie x(300.0, 30.0,40.0 ,50.0, 2.0);
-    //std::cout << x; // testam operatorul <<
     return 0;
 }
